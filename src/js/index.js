19ae4b56ab543ab999340ba59db051c673ebc46e
usukesato <y.sato@lineaworks.jp>
2021-10-28 14:14:08 +0900

    PRODCUTリストとモーダルの実装 指摘点の修正(h1 -> h2, font-size, font-weight, margin等)

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,14 @@ import { debounce } from 'lodash-es';
 import Stats from 'stats-js';
 import jquery from 'jquery';
 
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper/swiper-bundle';
+import 'swiper/swiper-bundle.min.css';
+
 import EVENTS from '~/constants/event-names';
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
 
 const $ = jquery;
 
@@ -40,17 +47,58 @@ const $ = jquery;
   }
   window.addEventListener(EVENTS.LOAD, onLoad);
 
+  // products swiper
+  const productsSwiper = new Swiper('.product-modal-slider', {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.product-modal-next',
+      prevEl: '.product-modal-prev',
+    },
+  });
+
+  // image swiper
+  $('.product-modal-img').each(function (i) {
+    const sliderNum = $(this).data('sliderNum');
+    const imageSwiper = new Swiper(
+      `.product-modal-img-slider.slider-num-${sliderNum}`,
+      {
+        nested: true,
+        resistanceRatio: 0,
+        pagination: {
+          el: `.product-modal-img-pagination.slider-num-${sliderNum}`,
+          type: 'fraction',
+        },
+        navigation: {
+          nextEl: `.product-modal-img-next.slider-num-${sliderNum}`,
+          prevEl: `.product-modal-img-prev.slider-num-${sliderNum}`,
+        },
+      }
+    );
+  });
+
   // menu
   $('.header-menu-button').on('mouseenter', function () {
-    $(this).addClass('on');
+    $(this).addClass('is-on');
   });
   $('.header-menu-button').on('mouseleave', function () {
-    $(this).removeClass('on');
+    $(this).removeClass('is-on');
   });
   $('.header-menu-button').on('click', function () {
-    $('header').toggleClass('click');
-    $(this).toggleClass('click');
-    $(this).next('.header-menu-overlay').toggleClass('on');
+    $('header').toggleClass('is-click');
+    $(this).toggleClass('is-click');
+    $(this).next('.header-menu-overlay').toggleClass('is-on');
+  });
+
+  // products modal
+  $('.product-list-item').on('click', function () {
+    const sliderNum = $(this).data('sliderNum');
+    productsSwiper.slideTo(sliderNum);
+    $('.product-modal').addClass('is-on');
+  });
+
+  // products modal close
+  $('.product-modal-close').on('click', function () {
+    $('.product-modal').removeClass('is-on');
   });
 
   // PAGE TOP
